579ec5cff1359dd564f529ad0b0e81cc3819e90c
Jingdong <mxuan@ucla.edu>
2020-02-19 21:45:06 -0800

    modified files to use negative test set

--- Equation/neg_training_set.py
+++ Equation/neg_training_set.py
@@ -50,44 +50,41 @@ def main():
     c = 0
     
     for l in lines:
-        #print(c)
+        print(c)
         c += 1
         seq_encode = []
         raw_eqs = l[0: -1].split(';')[0: -1]
         h_len = len(raw_eqs)
 
-        j = 0
         for i in range(h_len-1):
+            j = 0
             equation = str_tuple(raw_eqs[i])
-            next_states_list = [eq.tuple2str(t) for  t in next_states(equation)]
+            next_states_list = [eq.tuple2str(t[0]) + t[1] for  t in next_states(equation)]
             while True:
                 index = np.random.choice(len(next_states_list), 1)
                 temp = next_states_list[index[0]]
-                if temp != raw_eqs[i+1][:-1]:
-                    seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[i][:-1])])
+                if temp != raw_eqs[i+1]:
+                    seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[i])])
+                    #print(temp)
                     break
-                #print(raw_eqs[i][:-1])
-                #print(next_states_list)
-                #print()
-                print(j)
-                j+=1
+
         for i in range(int(h_len/2), h_len-1):
             equation = str_tuple(raw_eqs[i])
-            next_states_list = [eq.tuple2str(t) for  t in next_states(equation)]
+            next_states_list = [eq.tuple2str(t[0]) + t[1] for  t in next_states(equation)]
             while True:
                 index = np.random.choice(len(next_states_list), 1)
                 temp = next_states_list[index[0]]
-                if temp != raw_eqs[i+1][:-1]:
-                    seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[i][:-1])])
+                if temp != raw_eqs[i+1]:
+                    seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[i])])
                     break
                 
         equation = str_tuple(raw_eqs[h_len-1])
-        next_states_list = [eq.tuple2str(t) for  t in next_states(equation)]
+        next_states_list = [eq.tuple2str(t[0]) + t[1] for t in next_states(equation)]
         temp = next_states_list[np.random.choice(len(next_states_list), 1)[0]]
-        seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[h_len-1][:-1])])
+        seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[h_len-1])])
         temp = next_states_list[np.random.choice(len(next_states_list), 1)[0]]
-        seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[h_len-1][:-1])])
-            
+        seq_encode.append([eq.encode(temp), eq.encode(raw_eqs[h_len-1])])
+        #print(seq_encode)    
         seq_encodes.append(seq_encode)
     seq_encodes = np.array(seq_encodes)
     np.save('../Data/neg_training_set_2_4_20_5.npy', seq_encodes)
