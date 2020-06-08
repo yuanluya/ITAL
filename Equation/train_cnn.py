25b0cd2896c456443d2f19bed74c5a05b09ebd90
Junhong Shen <48073240+sjunhongshen@users.noreply.github.com>
2020-06-08 15:07:54 -0700

    Update train_cnn.py

--- Equation/train_cnn.py
+++ Equation/train_cnn.py
@@ -19,13 +19,15 @@ import pdb
 def main():
     np.random.seed(1234)
     batch_size = 128
-    lr = 5e-5
-    codebook_ = np.array([str(digit) for digit in range(10)] + ['+', '-', '/', '^', '='] + ['x', 'y'] + [' '] + ['!'])
+    lr = 1e-4
+    codebook_ = np.array([str(digit) for digit in range(10)] + ['+', '-', '/', '^', '='] + ['x', 'y'] + [' '] + [''])
+    op_type = ["scale", "merge", "remove denominators", "sort"]
     
-    train_data = np.load('/home/Datasets/equation_100/equations_encoded_2_4_20_5.npy', allow_pickle = True)[:90000]
-    test_data = np.load('/home/Datasets/equation_100//equations_encoded_2_4_20_5.npy', allow_pickle = True)[90000:]
-    neg_train_data = np.load('/home/Datasets/equation_100/neg_training_set_2_4_20_5.npy', allow_pickle = True)[:90000]
-    neg_test_data = np.load('/home/Datasets/equation_100/neg_training_set_2_4_20_5.npy', allow_pickle = True)[90000:]
+    train_data = np.load('/home/Datasets/equation_3_4_20_5/equations_encoded_3_4_20_5.npy', allow_pickle = True)[:490000]
+    test_data = np.load('/home/Datasets/equation_3_4_20_5/equations_encoded_3_4_20_5.npy', allow_pickle = True)[490000:]
+
+    neg_train_data = np.load('/home/Datasets/equation_3_4_20_5/neg_training_set_3_4_20_5.npy', allow_pickle = True)[:490000]
+    neg_test_data = np.load('/home/Datasets/equation_3_4_20_5/neg_training_set_3_4_20_5.npy', allow_pickle = True)[490000:]
     print("Train data size pos: %d neg: %d" % (len(train_data), len(neg_train_data)))
     print("Test data size pos: %d neg: %d" % (len(test_data), len(neg_test_data)))  
 
@@ -33,14 +35,14 @@ def main():
     higher_equations_all = []
     train_pos_idx = []
     train_neg_idx = []
-    for d in train_data:
+    for d in tqdm(train_data):
         for i in range(len(d)):
             if i != len(d) - 1:
                 lower_equations_all.append(d[i][:-1])
             if i != 0:
                 higher_equations_all.append(d[i][:-1])
                 train_pos_idx.append(d[i][-1])
-    for d in neg_train_data:
+    for d in tqdm(neg_train_data):
         for i in range(len(d)):
             lower_equations_all.append(d[i][0][:-1])
             higher_equations_all.append(d[i][1][:-1])
@@ -60,7 +62,7 @@ def main():
     higher_equations_test = []
     test_pos_idx = []
     test_neg_idx = []
-    for d in test_data:
+    for d in tqdm(test_data):
         for i in range(len(d)):
             if i != len(d) - 1:
                 lower_equations_test.append(d[i][:-1])
@@ -68,7 +70,7 @@ def main():
                 higher_equations_test.append(d[i][:-1])
                 test_pos_idx.append(d[i][-1])
     pos_test = len(lower_equations_test)
-    for d in neg_test_data:
+    for d in tqdm(neg_test_data):
         for i in range(len(d)):
             lower_equations_test.append(d[i][0][:-1])
             higher_equations_test.append(d[i][1][:-1])
@@ -82,10 +84,10 @@ def main():
     M = max(M0, M1)
     print("Max length: %d" % M)
 
-    eqv_config = edict({'input_dim': M, 'encoding_dim': 20, 'output_dim': 30, 'C': 1, 'reg_param': 1e-5, 'batch_size': batch_size,
-        'lr': lr, 'num_character': 18, 'layer_info': [(64, 3, 1), (32, 3, 1), (16, 3, 1)]})
+    eqv_config = edict({'input_dim': M, 'encoding_dim': 30, 'output_dim': 40, 'C': 1, 'reg_param': 1e-5, 'batch_size': batch_size,
+        'lr': lr, 'num_character': 19, 'layer_info': [(64, 5, 1, False), (64, 5, 1, True), (32, 3, 1, False), (32, 3, 1, True), (32, 3, 1, False), (32, 3, 1, True)]})
     
-    init_w = np.random.uniform(size = [1, eqv_config.output_dim])
+    init_w = np.random.uniform(size = [1, eqv_config.output_dim + 1])
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
@@ -93,7 +95,7 @@ def main():
     
     init = tf.global_variables_initializer()
     sess.run(init)
-    ckpt_dir = 'CKPT_cnn_dim_%d_encoding_dim_%d_2_4_all' % (eqv_config.output_dim, eqv_config.encoding_dim)
+    ckpt_dir = 'CKPT_cnn_dim_%d_encoding_dim_%d_3_4_6layers' % (eqv_config.output_dim, eqv_config.encoding_dim)
     eqv.restore_ckpt(ckpt_dir)
 
     ###################### train ############################
@@ -133,7 +135,9 @@ def main():
     accuracy = []
     loss = []
     accuracy_by_type = []
-    for itr in range(int(len(lower_equations_test) / batch_size)):
+    train_features = np.ndarray((0, eqv_config.output_dim))
+    train_labels = np.ndarray((0))
+    for itr in tqdm(range(int(len(lower_equations_test) / batch_size))):
         lower_equations = lower_equations_test[itr * batch_size: (itr + 1) * batch_size]
         higher_equations = higher_equations_test[itr * batch_size: (itr + 1) * batch_size]
 
@@ -144,19 +148,35 @@ def main():
         lower_encoding_idx = np.expand_dims(list(range(eqv_config.batch_size)), axis = 1)
         higher_encoding_idx = np.expand_dims(list(range(eqv_config.batch_size, 2 * eqv_config.batch_size, 1)), axis = 1)
 
-        w, l, lower_vals, higher_vals, lay, enc = eqv.sess_.run([eqv.weight_, eqv.loss_, eqv.lower_vals_, eqv.higher_vals_, eqv.layers_, eqv.encodings_], \
+        w, l, lower_vals, higher_vals, lenc, henc = eqv.sess_.run([eqv.weight_, eqv.loss_, eqv.lower_vals_, eqv.higher_vals_, eqv.lower_encodings_0_, eqv.higher_encodings_0_], \
                                     {eqv.lower_eqs_idx_: lower_eqs_idx, eqv.higher_eqs_idx_: higher_eqs_idx,
                                     eqv.lower_encoding_idx_: lower_encoding_idx, eqv.higher_encoding_idx_: higher_encoding_idx})
         accuracy.append(np.mean(lower_vals < higher_vals))
         accuracy_by_type.append(lower_vals < higher_vals)
         loss.append(l)
-    
+        train_features = np.concatenate((train_features, lenc))
+        train_labels = np.concatenate((train_labels, lower_vals))
+        train_features = np.concatenate((train_features, henc))
+        train_labels = np.concatenate((train_labels, higher_vals))
+        
+        # for a in range(batch_size):
+        #     if (lower_vals < higher_vals)[a] == 0:
+        #         str1 = codebook_[lower_equations[a]]
+        #         str2 = codebook_[higher_equations[a]]
+        #         print("\nOperation type: %s" % op_type[np.concatenate((test_pos_idx, test_neg_idx))[itr * batch_size + a] - 22])
+        #         print(''.join(str1))
+        #         print(''.join(str2))
+
+    np.save("equation_train_features_cnn_3var_40_6layers.npy", train_features[1:])
+    np.save("equation_train_labels_cnn_3var_40_6layers.npy", train_labels[1:])
+    np.save("equation_gt_weights_cnn_3var_40_6layers.npy", w)
+
     print("Test accuracy: %f loss: %f" % (np.mean(accuracy), np.mean(loss)))
     print("Weight: %s" % w)
     accuracy_by_type_pos = np.array(accuracy_by_type).flatten()[:len(test_pos_idx)]
     accuracy_by_type_neg = np.array(accuracy_by_type).flatten()[len(test_pos_idx):]
     test_neg_idx = np.array(test_neg_idx[:len(np.array(accuracy_by_type).flatten()) - len(test_pos_idx)])
-    op_type = ["scale", "merge", "remove denominators", "sort"]
+    
     for i in range(22, 26):
         print("Operation type: %s" % op_type[i - 22])
         print("\tpos accuracy: %f size: %d" % (np.mean(accuracy_by_type_pos[test_pos_idx == i]), np.sum(test_pos_idx == i)))
